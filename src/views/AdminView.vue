2a231e1b374c746fe11f8e2324f72855a2bee660
admin <admin@MacBook-Pro-2.local>
2022-05-09 04:46:25 +0200

    sidebar

---
+++ src/views/AdminView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <SideBar/>
+  </div>
+</template>
+
+<script>
+
+import SideBar from "@/components/layouts/SideBar";
+
+export default {
+  name: "AdminView",
+  components: {
+    SideBar
+  }
+}
+</script>
+
